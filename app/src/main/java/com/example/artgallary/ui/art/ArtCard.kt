e7846d701f38e519e09db17e5c8e4672fc5c4cb4
Maryam <mal7othify@gmail.com>
2021-09-11 20:00:56 +0300

    Enhance visibility animation

--- app/src/main/java/com/example/artgallary/ui/art/ArtCard.kt
+++ app/src/main/java/com/example/artgallary/ui/art/ArtCard.kt
@@ -3,6 +3,10 @@ package com.example.artgallary.ui.art
 import android.content.res.Configuration
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.expandIn
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.shrinkOut
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
@@ -14,6 +18,9 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -21,6 +28,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.imageResource
@@ -52,7 +60,9 @@ fun ArtCard(image: Int, title: String, artist: String, desc: String) {
             Image(
                 contentDescription = null,
                 bitmap = ImageBitmap.imageResource(image),
-                modifier = Modifier.requiredHeight(200.dp).fillMaxWidth(),
+                modifier = Modifier
+                    .requiredHeight(200.dp)
+                    .fillMaxWidth(),
                 contentScale = ContentScale.Crop
             )
             Text(
@@ -63,11 +73,24 @@ fun ArtCard(image: Int, title: String, artist: String, desc: String) {
             Text(
                 text = "by $artist",
                 style = MaterialTheme.typography.body2,
-                modifier = Modifier.padding(top = 8.dp, bottom = 8.dp),
+                modifier = Modifier.padding(vertical = 8.dp),
                 textAlign = TextAlign.Right,
                 color = MaterialTheme.colors.primary
             )
-            AnimatedVisibility(expandedState) {
+            Image(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.CenterHorizontally)
+                    .padding(bottom = 8.dp),
+                imageVector = if (expandedState) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = null,
+                colorFilter = ColorFilter.tint(MaterialTheme.colors.onSurface)
+            )
+            AnimatedVisibility(
+                visible = expandedState,
+                enter = expandIn() + expandVertically(),
+                exit = shrinkOut() + shrinkVertically(),
+            ) {
                 Text(
                     text = desc,
                     style = MaterialTheme.typography.body1,
