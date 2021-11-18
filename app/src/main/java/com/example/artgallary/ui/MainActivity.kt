447541f32a7e769ec29d34bc4017881544ad5768
Maryam Alhuthayfi <mal7othify@gmail.com>
2021-11-18 23:20:08 +0300

    Prepare files for Material You

--- app/src/main/java/com/example/artgallary/ui/MainActivity.kt
+++ app/src/main/java/com/example/artgallary/ui/MainActivity.kt
@@ -1,22 +1,22 @@
 package com.example.artgallary.ui
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material3.MaterialTheme
 import com.example.artgallary.model.Arts
 import com.example.artgallary.ui.arts.AllArts
 import com.example.artgallary.ui.theme.ArtGallaryTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ArtGallaryTheme {
-                Surface(color = MaterialTheme.colors.background) {
+                Surface(color = MaterialTheme.colorScheme.inverseOnSurface) {
                     AllArts(Arts)
                 }
             }
--- app/src/main/java/com/example/artgallary/ui/theme/Theme.kt
+++ app/src/main/java/com/example/artgallary/ui/theme/Theme.kt
@@ -1,38 +1,60 @@
 package com.example.artgallary.ui.theme
 
+import android.app.Activity
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.ViewCompat
 
-private val DarkColorPalette = darkColors(
+private val DarkColorScheme = darkColorScheme(
     primary = lightGray,
-    primaryVariant = darkGray,
-    secondary = teal200,
-    onBackground = Color.White
+    secondary = darkGray,
+    tertiary = teal200,
+    onBackground = lightGray,
+    onSecondary = darkGray
 )
 
-private val LightColorPalette = lightColors(
+private val LightColorScheme = lightColorScheme(
     primary = lightGray,
-    primaryVariant = darkGray,
-    secondary = teal200,
+    secondary = darkGray,
+    tertiary = teal200,
     onBackground = Color.DarkGray
 )
 
 @Composable
-fun ArtGallaryTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
+fun ArtGallaryTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = true,
+    content: @Composable() () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
+            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+        }
     }
 
-    MaterialTheme(
-        colors = colors,
-        typography = typography,
-        shapes = shapes,
+    androidx.compose.material3.MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
         content = content
     )
 }
