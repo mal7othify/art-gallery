68603b9be9c8fc40ccb1e5f0d6297c3b7b9e555f
Maryam Alhuthayfi <mal7othify@gmail.com>
2021-11-29 23:33:06 +0300

    Update colors

--- app/src/main/java/com/example/artgallary/ui/theme/Color.kt
+++ app/src/main/java/com/example/artgallary/ui/theme/Color.kt
@@ -3,5 +3,5 @@ package com.example.artgallary.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val darkGray = Color(0xFF37474F)
-val lightGray = Color(0xFF7395A5)
+val lightGray = Color(0xFF607D8B)
 val teal200 = Color(0xFF03DAC5)
